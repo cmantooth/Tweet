3497fa6eb628050ab9db3057365acfcb9be156fb
cmantooth <69709641+cmantooth@users.noreply.github.com>
2021-01-22 00:51:56 -0600

    add about page

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 def home():
     return 'Hello World!'
 
+@app.route('/about')
+def about():
+
+    about_text = """
+    This is a text analyzer
+    """
+    
+    return about_text
 
 if __name__=='__main__':
 
     app.run(debug=True)
+    
+
