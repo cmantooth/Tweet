eb0b61120c04df6e475975d9acb528a550f8487c
cmantooth <69709641+cmantooth@users.noreply.github.com>
2021-01-22 01:33:35 -0600

    added tweet text

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    
+    # if request has args (?tweet=some_tweet)
+    if request.args:
+        
+        # get the value of the arg
+        tweet = request.args.get('tweet')   
+        
+        return tweet
+    
     return 'Hello World!'
 
 @app.route('/about')
